3bce4c3853818ae0d477075c4956770783d638b1
Gayi02 <you@example.com>
2023-12-23 15:38:07 +0530

    Date and time

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -4,5 +4,5 @@ FRUITS=("Apples" "Banana" "Mango")
 
 echo "First value=${FRUITS[0]}"
 echo "Second value=${FRUITS[1]}"
-echo "Third value=${FRUITS[2}"
+echo "Third value=${FRUITS[2]}"
 echo "all value=${FRUITS[@]}"
