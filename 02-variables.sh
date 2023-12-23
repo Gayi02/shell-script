f2d3c57aed35d3189d7f0aa207a60e3ee2793639
Gayi02 <you@example.com>
2023-12-23 14:58:32 +0530

    variables script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Ramesh:Hello Suresh, How are you"
-echo "Suresh:Hi Ramesh. I am fine How are you?"
-echo "Ramesh:I am good.Where are you"
-echo "Suresh:I am in Bangalore"
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1:Hello $PERSON2, How are you"
+echo "$PERSON2:Hi $PERSON1. I am fine How are you?"
+echo "$PERSON1:I am good.Where are you"
+echo "$PERSON2:I am in Bangalore"
