dc7ae779fcc32fecf6f9905d0e705c9d00f0cf42
Gayi02 <you@example.com>
2023-12-24 23:29:29 +0530

    if condition

--- 11-log.sh
+++ 11-log.sh
@@ -2,6 +2,9 @@
 
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
@@ -10,16 +13,16 @@ echo "script name: $0"
 VALIDATE(){
     if [ $? -ne 0 ]
     then
-       echo "Error: $2... failed"
+       echo -e "Error: $2... $R failed $N"
        exit 1
     else
-       echo "$2... success"
+       echo -e "$2... $G success $N"
     fi   
 }
 
 if [ $ID -ne 0 ]
 then
-   echo "Error: Please run this script with root access"
+   echo -e "$R Error: Please run this script with root access $N"
    exit 1
 else
    echo "you are a root user"
