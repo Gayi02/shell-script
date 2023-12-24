5c0026837670dca098f7b4169c234931bf32dbbf
Gayi02 <you@example.com>
2023-12-24 22:53:24 +0530

    if condition

---
+++ 10-function.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then
+       echo "Error: $2... failed"
+       exit 1
+    else
+       echo "$2... success"
+    fi   
+}
+
+if [ $ID -ne 0 ]
+then
+   echo "Error: Please run this script with root access"
+   exit 1
+else
+   echo "you are a root user"
+fi
+
+yum install mysql -y
+
+VALIDATE $? "Installing mysql"
+
+yum install git -y
+
+VALIDATE $? "Installing git"
