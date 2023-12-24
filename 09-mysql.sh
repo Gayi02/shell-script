6fadd87bb14537005f21ece8ae7a1aa02333f4b5
Gayi02 <you@example.com>
2023-12-24 22:30:53 +0530

    if condition

--- 09-mysql.sh
+++ 09-mysql.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-ID=($id -u)
+ID=$(id -u)
 
 if [ $ID -ne 0 ]
 then
-echo "Error: Please run this script with root access"
-exit 1
+   echo "Error: Please run this script with root access"
+   exit 1
 else
-echo "you are a root user"
+   echo "you are a root user"
 fi
 
 yum install mysql -y
